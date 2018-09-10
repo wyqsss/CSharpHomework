6f4ae98300e0dc560ddbc14d54cf3d19b68baf5a
12464 <12464@DESKTOP-LALQMBM>
2018-09-10 17:21:04 +0800

    添加项目文件。

---
+++ program1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "";
+            string s2 = "";
+            double a = 0;
+            double b;
+            double c;
+            Console.WriteLine("Please input 2 number :");
+            s1 = Console.ReadLine();
+            a = Double.Parse(s1);
+            s2 = Console.ReadLine();
+            b = Double.Parse(s2);
+            c = a * b;
+            Console.WriteLine(a+"*"+b+"="+c);
+        }
+    }
+}
