d529e5110576f71c5f2131c9a8f937c22c65fcf3
wyqsss <43110033+wyqsss@users.noreply.github.com>
2018-11-29 20:22:01 +0800

    Add files via upload

---
+++ homework_10/OrderService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace program1
+{
+    public class OrderService
+    {
+        public void Add(Order order)
+        {
+            using (var data_1 = new ordersql())
+            {
+                data_1.Order.Add(order);
+                data_1.SaveChanges();
+            }
+        }
+
+        public void Delete(String orderId)
+        {
+            using (var data_1 = new ordersql())
+            {
+                var order = data_1.Order.Include("Details").SingleOrDefault(o => o.Id.ToString() == orderId);
+                data_1.OrderDetails.RemoveRange(order.Details);
+                data_1.Order.Remove(order);
+                data_1.SaveChanges();
+            }
+        }
+
+        public void Update(Order order)
+        {
+            using (var data_1 = new ordersql())
+            {
+                data_1.Order.Attach(order);
+                data_1.Entry(order).State = EntityState.Modified;
+                order.Details.ForEach(
+                    Details => data_1.Entry(Details).State = EntityState.Modified);
+                data_1.SaveChanges();
+            }
+        }
+
+        public Order GetOrder(String Id)
+        {
+            using (var data_1 = new ordersql())
+            {
+                return data_1.Order.Include("Details").
+                  SingleOrDefault(o => o.Id.ToString() == Id);
+            }
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            using (var data_1 = new ordersql())
+            {
+                return data_1.Order.Include("Details").ToList<Order>();
+            }
+        }
+
+    }
+}
---
+++ homework_10/ordersql.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class ordersql:DbContext
+    {
+        public ordersql() : base("OrderDataBase") { }
+
+        public DbSet<Order> Order { get; set; }
+
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+
+        public DbSet<Goods> goods { get; set; }
+    }
+}
